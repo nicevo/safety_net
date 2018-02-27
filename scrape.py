cfbbbc4b3e8d1c15d55cef7b1eee810ceb500822
Mr User <29448170+aexg@users.noreply.github.com>
2018-02-27 16:39:31 +0100

    Refactor keeping the existing functionality
    - Regroup together function definitions and functions calls.
    - Remove debug code for parts that are already debugged.
    - Use python3 style string formating.

--- scrape.py
+++ scrape.py
@@ -1,45 +1,14 @@
-# import some Python dependencies
+# scrape.py
+#
+# Module in Safety Net project
+# Purpose: to retrieve messages and statistics for posts in feed of a Facebook page given as input.
 
 import datetime
 import json
-import time
 import os
-from urllib.request import urlopen
-
 import requests
-
-
-try:
-    app_id = os.environ['FB_APP_ID']
-    app_secret = os.environ['FB_APP_SECRET']
-except KeyError:
-    print('ERROR: Please define environment variables FB_APP_ID and FB_APP_SECRET.')
-    exit(1)
-
-
-access_token = app_id + "|" + app_secret
-
-page_id = 'nytimes'
-
-
-def testFacebookPageData(page_id, access_token):
-    # construct the URL string
-    base = "https://graph.facebook.com/v2.4"
-    node = "/" + page_id
-    parameters = "/?access_token=%s" % access_token
-    url = base + node + parameters
-
-    # retrieve data
-    response = urlopen(url)
-    d = response.read().decode('utf8')
-    print(d)
-    data = json.loads(d)
-
-    print
-    json.dumps(data, indent=4, sort_keys=True)
-
-
-testFacebookPageData(page_id, access_token)
+import time
+from urllib.request import urlopen
 
 
 def request_until_succeed(url):
@@ -51,40 +20,19 @@ def request_until_succeed(url):
                 success = True
         except (Exception):
             time.sleep(5)
-
             print("Error for URL %s: %s" % (url, datetime.datetime.now()))
 
     return response.read().decode('utf8')
 
 
-def testFacebookPageFeedData(page_id, access_token):
-    # construct the URL string
-    base = "https://graph.facebook.com/v2.4"
-    node = "/" + page_id + "/feed"  # changed
-    parameters = "/?access_token=%s" % access_token
-    url = base + node + parameters
-
+def getFacebookPageFeedData(page_id, msg_limit, access_token):
+    # construct the URL string from template
+    url_template = ('https://graph.facebook.com/{}/feed/?fields=message,link,created_time,type,name,id,'
+        'likes.limit(1).summary(true),comments.limit(1).summary(true),shares&'
+        'limit={}&access_token={}')
+    url = url_template.format(page_id, msg_limit, access_token)
     # retrieve data
     data = json.loads(request_until_succeed(url))
-
-    print
-    json.dumps(data, indent=4, sort_keys=True)
-
-
-# testFacebookPageFeedData(page_id, access_token)
-
-
-def getFacebookPageFeedData(page_id, access_token, num_statuses):
-    # construct the URL string
-    base = "https://graph.facebook.com"
-    node = "/" + page_id + "/feed"
-    parameters = "/?fields=message,link,created_time,type,name,id,likes.limit(1).summary(true),comments.limit(1).summary(true),shares&limit=%s&access_token=%s" % (
-        num_statuses, access_token)  # changed
-    url = base + node + parameters
-
-    # retrieve data
-    data = json.loads(request_until_succeed(url))
-
     return data
 
 
@@ -94,14 +42,24 @@ def isAcceptable(text):
     return r.json()['results']['value'] == 'Non-Adult'
 
 
-test_status = getFacebookPageFeedData(page_id, access_token, 3)["data"]
+if __name__ == "__main__":
+    try:
+        app_id = os.environ['FB_APP_ID']
+        app_secret = os.environ['FB_APP_SECRET']
+    except KeyError as e:
+        print('ERROR: Please define environment variables FB_APP_ID and FB_APP_SECRET.')
+        exit(1)
 
-for m in test_status:
-    message = m["message"]
-    likes = m["likes"]["summary"]["total_count"]
-    shares = m["shares"]["count"]
-    # print(json.dumps(message, indent=4, sort_keys=True))
-    print("message:", message)
-    print("shares:", shares)
-    print("likes:", likes)
-    print("isAcceptable", isAcceptable(message))
+    access_token = app_id + "|" + app_secret
+    page_id = 'nytimes'
+
+    test_status = getFacebookPageFeedData(page_id, 3, access_token)["data"]
+    for m in test_status:
+        message = m["message"]
+        likes = m["likes"]["summary"]["total_count"]
+        shares = m["shares"]["count"]
+        print("message:", message)
+        print("shares:", shares)
+        print("likes:", likes)
+        print("isAcceptable", isAcceptable(message))
+
